167645838366793a31c9a72973a2f7aee1ed7da5
gopalreddypulikanti <gopalpulikanti33@gmail.com>
2025-03-20 16:53:29 +0530

    Create applications.py

---
+++ applications.py
@@ -0,0 +1,44 @@
+def add(x, y):
+  return x + y
+print(add(20, 30)
+
+
+
+def sub(x, y):
+  return x - y
+
+print(sub(20, 30)
+
+
+def mul(x, y):
+  return x * y
+
+print(mul(20, 30)
+
+
+      
+def div(x, y):
+  return x / y
+
+
+print(div(20,30)
+
+
+
+      
+      
+      
+      
+      
+
+      
+      
+      
+
+      
+
+
+
+      
+  
+
